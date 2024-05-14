6ea7cd2ce655e95ad0d7bc7c349c293e29658575
allagon763 <167851467+allagon763@users.noreply.github.com>
2024-05-14 12:57:54 +0200

    Add files via upload

--- TFG GitHub/index.js
+++ TFG GitHub/index.js
@@ -1,3 +1,5 @@
+    //BOTÓN DE WHATSAPP
+
 //Creamos una variable, a través de la cual obtenemos el boton de whatsapp con un getElementById 
 const whatsbtn = document.getElementById('whatsbtn');
 
@@ -8,3 +10,27 @@ whatsbtn.addEventListener('click', function() {
     window.open(link, '_blank');
 });
 
+    //NOMBRE DE LOS EMPLEADOS
+
+//Mostrar y ocultar nombre de los empleados
+function mostrarNombre(img) {
+    var nombreEmpleado = img.nextElementSibling; // De esta manera obtenemos el div con el nombre del empleado, ya que se selecciona el elemento hermano
+    nombreEmpleado.style.display="block"; // Así se muestra el nombre del empleado
+}
+
+function ocultarNombre(img) {
+    var nombreEmpleado = img.nextElementSibling; 
+    nombreEmpleado.style.display = "none"; // Así se oculta el nombre del empleado
+}
+
+    //CAMBIO COLOR OFERTAS
+
+//De esta manera cambiamos el color del texto al colocar el cursor encima
+let currentColorIndex = 0;
+const colors = ['#ff5733', '#33ff57', '#5733ff']; // Este es un array que contiene los colores que saldran cada vez que pongamos el cursos encima
+
+function cambiocolor() {
+    const texto = document.querySelector('.ofertatext');
+    texto.style.color = colors[currentColorIndex];
+    currentColorIndex = (currentColorIndex + 1) % colors.length; // De esta manera avanza al siguiente color circularmente (basicamente, si no colocamos esto no cambian los colores)
+}
